b0a84f8d272f1eb47faebdd136931c819308c928
jjjortega <jjjortega@gmail.com>
2018-03-22 23:14:22 -0400

    Ya creados los modelos, las migraciones, los factories, los seeders.

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'slug', 
+    ];
+    
+    public function Posts(){
+        return $this->belongsToMany(Post::class);
+    }
 }
